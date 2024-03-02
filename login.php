b33c03647383a4adc242a407c1cd97edf5767d8a
wnzyy <indahdwi3753@gmail.com>
2024-03-02 10:24:21 +0700

    latihan

--- login.php
+++ login.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login</title>
+    
 </head>
 <body>
     <h1>asdkfjhkmasbdifkujsadkfbiasdbfkmnb</h1>
+    <h2>latihan</h2>
 </body>
 </html>                   
